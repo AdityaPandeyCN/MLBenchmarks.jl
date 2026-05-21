84404fa5087bc650fa0d7d1b519c37ebe7453780
AdityaPandeyCN <adityapand3y666@gmail.com>
2026-05-21 16:10:17 +0530

    up
    Signed-off-by: AdityaPandeyCN <adityapand3y666@gmail.com>

--- benchmarks/year.jl
+++ benchmarks/year.jl
@@ -6,18 +6,21 @@ using CSV
 using Zygote
 
 data_name = :year
-modernnca_hyper_size = 8
+modernnca_hyper_size = 2
 data = load_data(data_name; uniformize=true)
 metrics = [:mse, :rmse]
 mkpath(joinpath("results", string(data_name)))
 
 ################################
-# ModernNCA
+# NeuroTabModels ModernNCA
 ################################
-hyper_list = MLBenchmarks.get_hyper_modernnca(modernnca_hyper_size; data.loss, data.metric, device=:gpu, backend=:zygote, nrounds=200, early_stopping_rounds=2, lr=1e-3, d_embedding=[64, 128], d_block=[128, 256], n_blocks=[1, 2], dropout=0.1, temperature=1.0, sample_rate=0.01, batchsize=1024, seed=123)
+hyper_list = MLBenchmarks.get_hyper_modernnca(modernnca_hyper_size; data.loss, data.metric, device=:gpu, backend=:zygote, nrounds=200, early_stopping_rounds=2, lr=1e-3, d_embedding=[64, 128], d_block=128, n_blocks=0, dropout=0.1, temperature=1.0, sample_rate=0.01, batchsize=1024, seed=123)
 results_df = run_experiment(:NeuroTabModels, data, hyper_list; metrics)
 CSV.write(joinpath("results", string(data_name), "modernnca.csv"), results_df)
 
-hyper_list = MLBenchmarks.get_hyper_talent_modernnca(modernnca_hyper_size; nrounds=200, lr=1e-3, dim=[64, 128], d_block=[128, 256], n_blocks=[1, 2], dropout=0.1, temperature=1.0, sample_rate=0.01, batchsize=1024, seed=123, normalization="none")
+################################
+# TALENT ModernNCA
+################################
+hyper_list = MLBenchmarks.get_hyper_talent_modernnca(modernnca_hyper_size; nrounds=200, lr=1e-3, dim=[64, 128], d_block=128, n_blocks=0, dropout=0.1, temperature=1.0, sample_rate=0.01, batchsize=1024, seed=123, normalization="none")
 results_df = run_experiment(:TALENT, data, hyper_list; metrics, save_root=joinpath("results", string(data_name), "talent_checkpoints"))
 CSV.write(joinpath("results", string(data_name), "talent_modernnca.csv"), results_df)
